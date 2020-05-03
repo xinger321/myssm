cc24130a1e2355d04274dd017eb04d972ccfe24c
lxg <xinger321@126.com>
2020-05-03 09:42:31 +0800

    ssm推送github

---
+++ src/main/java/com/mooc/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.mooc.controller;
+
+import com.mooc.bean.Demo;
+import com.mooc.service.DemoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+    @Autowired
+    private DemoService demoService;
+    
+    @RequestMapping("/hello/{id}")
+    @ResponseBody
+    public String hello(@PathVariable(value = "id") Long id){
+        return Optional.ofNullable(demoService.getById(id)).map(Demo::toString).orElse("empty string");
+        
+    }
+}
---
+++ src/main/java/com/mooc/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.mooc.service;
+
+import com.mooc.bean.Demo;
+import com.mooc.mapper.DemoMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DemoService {
+    @Autowired
+    private DemoMapper demoMapper;
+    
+    public Demo getById(Long id){
+        return Optional.ofNullable(demoMapper.selectByPrimaryKey(id)).orElse(null);
+    }
+}
